4a1d2b8c4b94d725a048871011061acb42bbe178
Optimus5734 <sagarverma5734@gmail.com>
2023-09-26 13:19:43 +0530

    Backend stuff

--- backend/index.js
+++ backend/index.js
@@ -2,108 +2,32 @@ import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import  booksRoute  from "./routes/booksRoute.js";
+
 
 const app = express();
+
 //Middleware to parse request body in json
 app.use(express.json());
+
+//Middleware for handling CORS policy
+//option-1 Allow all origins with default of cors(*)
+//app.use(cors(*));
+//option-2 Allow custom Origins
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type']
+}))
+
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome bhaiii");
 });
 
-//Route for Save a new Book
-app.post("/books", async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response
-        .status(400)
-        .send({ message: "Send all the needed field" });
-    }
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear,
-    };
-    const book = await Book.create(newBook);
-    return response.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//route to get all books from database
-app.get("/books", async (request, response) => {
-  try {
-    const books = await Book.find({});
-    return response.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//route to get one book from database by the us of id
-app.get("/book/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-    const book = await Book.findById(id);
-    if(!book){
-        return response.status(404).json({message:"Book not found"});
-    }
-    return response.status(200).json({ book });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//To Update the neccessary stuff
-app.put("/book/:id", async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response
-        .status(400)
-        .send({ message: "Send all the needed field" });
-    }
-    const { id } = request.params;
-    const result = await Book.findByIdAndUpdate(id,request.body);
-    if(!result){
-        return response.status(404).json({message:"Book not found"});
-    }
-    return response.status(200).json({ message:"Hey we have updated your request buddy" });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//to delete the require stuff
-app.delete("/book/:id", async (request, response) => {
-    try {
-        const { id } = request.params;
-        const book = await Book.findByIdAndDelete(id);
-        if(!book){
-            return response.status(404).json({message:"Book not found"});
-        }
-        return response.status(200).json({ message:"Bingo the book is been deleted" });
-      } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-      }
-  });
+// any incoming requests that start with /books will be handled by the booksRoute router.
+app.use('/books',booksRoute);
 
 mongoose
   .connect(mongoDBURL)
--- read.md
+++ read.md
@@ -1,4 +1,21 @@
 1. Created the normal server using index.js and config file 
 2.connect to mongo
+
+Note:-app.use() is an Express method used for middleware registration.
+
 3.create schema and Save a new book with mongoose
 4.Implementation of CRUD operations
+5.Creating a route folder {
+    because:--
+    in general 40 routes code asmuming 5 routes for each This is 
+    We have created the routes because it is a gd practise to 
+    do that as if we have say 5 modesls then we need to write 
+}
+
+6.Handeling cors policy
+
+7.created the frontend from npm create vite@latest
+{
+    Vite is a build tool and development server that is primarily designed for building web applications using modern JavaScript frameworks like Vue.js and React. It aims to provide a faster and more efficient development experience compared to traditional build tools, especially during development and testing phases.
+}
+now use of tailwind css
